41c7d33e152e94f3d7b86ae2dd7f31abc2dff873
Paul <paul.bartholomew08@imperial.ac.uk>
2018-08-24 16:28:27 +0100

    Removing old calls around variable-coeff Poisson equation

--- incompact3d.f90
+++ incompact3d.f90
@@ -373,23 +373,6 @@ PROGRAM incompact3d
                if ((nrank.eq.0).and.(poissiter.eq.0)) then
                   print *, "Solving variable-coefficient pressure-Poisson equation"
                endif
-               ! if (poissiter.ne.0) then
-               !   !! Compute correction term
-               !   call divergence_corr(rho1, px1, py1, pz1, ta1, tb1, tc1, td1, te1, tf1, di1, &
-               !        te2, tf2, ta2, tb2, tc2, td2, di2, &
-               !        td3, pp3corr, ta3, tb3, tc3, di3, rho0p3, pp3, tg3, &
-               !        nxmsize, nymsize, nzmsize, ph1, ph2, ph3, ph4, &
-               !        divup3norm, poissiter, converged)
-               ! else
-               !   !! Need an initial guess for 1/rho0 nabla^2 p - div( 1/rho nabla p )
-               !   call approx_divergence_corr(ux1, uy1, uz1, rho1, ta1, tb1, tc1, td1, te1, tf1, ep1, &
-               !        di1, rhos1, rhoss1, rhos01, rhos001, drhodt1, &
-               !        td2, te2, tf2, di2, ta2, tb2, tc2, &
-               !        ta3, tb3, tc3, di3, td3, te3, tf3, tg3, pp3corr, divu3, &
-               !        nxmsize, nymsize, nzmsize, ph1, ph3, ph4, &
-               !        divup3norm)
-               !   pp3corr(:,:,:) = pp3(:,:,:)
-               ! endif
                
                if (poissiter.eq.0) then
                   call calc_divup3norm(divup3norm, divu3, ta1, tb1, di1, ta2, tb2, tc2, di2, &
